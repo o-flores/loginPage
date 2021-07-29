5372909268f85083dd75c7469e6fde57cacb7630
Orlando Flores <orlando_flores@id.uff.br>
2021-07-29 19:55:53 -0300

    feat: provide AuthContext for entire App

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,18 @@ import { Route, Switch } from 'react-router-dom';
 import Login from './components/login/Login';
 import Create from './components/createAccount/Create';
 import Dashboard from './components/dashboard/Dashboard';
+import { AuthContextProvider } from './contexts/auth';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/create/account" component={Create} />
-        <Route exact path="/dashboard/:id" component={Dashboard} />
+        <AuthContextProvider>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/create/account" component={Create} />
+          <Route exact path="/dashboard/" component={Dashboard} />
+        </AuthContextProvider>
       </Switch>
     </div>
   );
