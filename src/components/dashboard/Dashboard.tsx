777bbf994c021291ed525b2c792d77c7ede66281
Orlando Flores <orlando_flores@id.uff.br>
2021-07-29 20:01:39 -0300

    feat: recover data from localStorage and redirects to homepage if user does not exist

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,9 +1,38 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 /* eslint-disable no-use-before-define */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useAuth } from '../../contexts/auth';
+import fetchUserInfo from '../../services/fetchUserInfo';
+import getUserInfoFromStorage from '../../services/getUserInfoFromStorage';
 
 function Dashboard() {
+  const [userFound, setUserFound] = useState(true);
+  const { user, setUser } = useAuth();
+
+  useEffect(() => {
+    async function fetchUser() {
+      const data = getUserInfoFromStorage();
+      if (!data?.account_id) setUserFound(false);
+      if (data) {
+        const userInfo = await fetchUserInfo(data.access_token, data.account_id);
+        setUser(userInfo);
+      }
+    }
+
+    fetchUser();
+  }, []);
+
+  if (!userFound) {
+    return (
+      <Redirect to="/" />
+    );
+  }
   return (
-    <p>dashboard</p>
+    <div>
+      {user?.email}
+    </div>
   );
 }
 
